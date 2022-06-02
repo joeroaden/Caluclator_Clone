fd56b843d14a020f1d2937183378825abb1e057d
Garrett Hays <GarrettlHays@gmail.com>
2022-06-02 10:26:36 -0700

    add user interface form functionality for js file
    Co-authored-by: Garrett Hays <garrettlhays@gmail.com>
    Co-authored-by: Joe Roaden <josephroaden@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,28 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    alert(result);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#output2").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#mult1").val());
+    const number2 = parseInt($("#mult2").val());
+    const result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#div1").val());
+    const number2 = parseInt($("#div2").val());
+    const result = divide(number1, number2);
+    $("#output4").text(result);
   });
 });
 
